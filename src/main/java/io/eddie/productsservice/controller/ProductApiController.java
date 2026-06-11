32369c8a847d5d86491f8ca6018cb1a0714cf99f
eddie1031 <archaco123@gmail.com>
2026-06-11 11:25:36 +0900

    init

--- src/main/java/io/eddie/productsservice/controller/ProductApiController.java
+++ src/main/java/io/eddie/productsservice/controller/ProductApiController.java
@@ -21,60 +21,30 @@ public class ProductApiController {
 
     private final ProductService service;
 
-    @Operation(
-            summary = "상품 추가 API",                      // API 요약
-            description = "상품을 추가하기 위한 API입니다."     // API 설명
-    )  // API 엔드포인트의 제목 및 설명 정의
-    @ApiResponse(
-            responseCode = "201",                       // 응답 코드
-            description = "저장된 상품에 대한 내용입니다.",    // 응답 설명
-            content = {                                 // 응답 상세정보
-                    @Content(
-                            mediaType = "application/json",          // 응답 컨텐츠 타입
-                            schema = @Schema(                        // 응답 본문 정보
-                                    implementation = Products.class  // 응답 본문 타입 참조
-                            )
-                    )
-            }
-    )  // API 응답에 대한 내용을 작성
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Products> save(ProductRequest request) {
         return ResponseEntity.ok(service.save(request));
     }
 
-    @Operation(
-            summary = "특정 상품 조회 API",
-            description = "상품 번호로 상품을 조회합니다."
-    )
+
     @GetMapping("/{id}")
     public ResponseEntity<Products> findById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
-    @Operation(
-            summary = "상품 목록 조회 API",
-            description = "상품 목록을 조회합니다."
-    )
+
     @GetMapping
     public ResponseEntity<List<Products>> findAll() {
         return ResponseEntity.ok(service.findAll());
     }
 
-    @Operation(
-            summary = "특정 상품 수정 API",
-            description = "상품 번호로 특정 상품을 수정합니다."
-    )
     @PatchMapping("/{id}")
     public ResponseEntity<Void> update(@RequestBody ProductRequest request, @PathVariable Long id) {
         service.update(request, id);
         return ResponseEntity.noContent().build();
     }
 
-    @Operation(
-            summary = "특정 상품 삭제 API",
-            description = "상품 번호로 특정 상품을 삭제합니다."
-    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
--- src/main/java/io/eddie/productsservice/dto/ProductRequest.java
+++ src/main/java/io/eddie/productsservice/dto/ProductRequest.java
@@ -10,28 +10,14 @@ public record ProductRequest(
 
         @NotBlank                       // Validation
         @Length(min = 10, max= 50)      // Validation
-        @Schema(
-            description = "상품 이름"  // 해당 필드에 대한 설명
-            , example = "상품 1"      // 해당 필드 예제값
-            , requiredMode = Schema.RequiredMode.REQUIRED  // 필드 필수 포함 여부
-            , minLength = 10        // 최소 길이
-            , maxLength = 50        // 최대 길이
-        )
         String name,
 
         @NotBlank
-        @Schema(description = "상품 설명", example = "상품 1에 대한 설명입니다.")
         String description,
 
 
         @NotNull
         @Min(value = 1000L)
-        @Schema(
-            description = "상품 가격"
-            , example = "판매하고자 하는 상품의 가격을 입력합니다."
-            , requiredMode = Schema.RequiredMode.REQUIRED
-            , minimum = "1000" // 최소값
-        )
         Long price
 
 ) {
